7bc79767cb334e31376fea40b949f17dfafb578d
hamm <aaron@HAM.local>
2018-10-08 00:05:36 -0700

    profile activity added

---
+++ app/src/main/java/com/example/aaron/budgeapp/Fragments/FragmentOne.java
@@ -0,0 +1,45 @@
+package com.example.aaron.budgeapp.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.example.aaron.budgeapp.MainActivity;
+import com.example.aaron.budgeapp.R;
+
+
+public class FragmentOne extends Fragment {
+
+    private ImageButton imageButton;
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/aaron/budgeapp/MainActivity.java
+++ app/src/main/java/com/example/aaron/budgeapp/MainActivity.java
@@ -1,32 +1,84 @@
 package com.example.aaron.budgeapp;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.aaron.budgeapp.Fragments.FragmentOne;
+import com.example.aaron.budgeapp.Fragments.FragmentTwo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView textView;
+    private ImageView imageView;
+
     private Button btnOCR;
+    private ImageButton btnP;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnP = findViewById(R.id.btnProfile);
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
         btnOCR = findViewById(R.id.btnOCR);
         btnOCR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startTextRecognitionActivity();
+
             }
         });
+
+        btnP = findViewById(R.id.btnProfile);
+        btnP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startProfileActivity();
+            }
+        });
+
+
+
+
     }
     public void startTextRecognitionActivity() {
         Intent intent = new Intent(MainActivity.this, TextRecognitionActivity.class);
         startActivity(intent);
 
     }
+
+    public void startProfileActivity(){
+        Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 
 }
